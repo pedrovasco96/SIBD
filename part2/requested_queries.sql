16689f96d0946028247ab31438621332cb79e561
João Rodrigues <joaorodrigues481@gmail.com>
2018-11-05 11:12:26 +0000

    fixed query 1 anf added 6 but with errors

--- part2/requested_queries.sql
+++ part2/requested_queries.sql
@@ -1,21 +1,29 @@
 -- 1
 -- change animal.vat to owner name
-SELECT animal.name, animal.VAT, animal.species_name, animal.age 
-FROM animal, consult, person 
-WHERE person.name='John Smith' AND person.VAT=consult.VAT_vet AND animal.VAT=consult.VAT_owner; 
+SELECT DISTINCT A.name, P2.name, A.species_name, A.age
+FROM animal A, consult C, person P1, person P2
+WHERE P1.name='John Smith' AND P1.VAT=C.VAT_vet AND A.VAT=C.VAT_owner AND P2.VAT = A.VAT;
 
 -- 2
-SELECT indicator.name, indicator.reference_value 
-FROM indicator 
-WHERE indicator.reference_value>100 AND indicator.units='milligrams' 
-ORDER BY indicator.reference_value DESC; 
+SELECT I.name, I.reference_value
+FROM indicator I
+WHERE I.reference_value>100 AND I.units='milligrams'
+ORDER BY I.reference_value DESC;
 
 -- 3
-SELECT DISTINCT animal.name, animal.VAT, animal.species_name, animal.age 
-FROM animal, consult
-WHERE animal.name=consult.name AND consult.weight>30 AND (consult.o='obese' OR consult.o='obesity'); 
+SELECT DISTINCT A.name, A.VAT, A.species_name, A.age
+FROM animal A, consult C
+WHERE A.name=C.name AND C.weight>30 AND (C.o='obese' OR C.o='obesity');
 
 -- 4
-SELECT person.VAT, person.name, person.adress_city, person.adress_street, person.adress_zip
-FROM person
-WHERE person.VAT NOT IN (SELECT animal.VAT FROM animal);
+SELECT P.VAT, P.name, P.adress_city, P.adress_street, P.adress_zip
+FROM person P
+WHERE P.VAT NOT IN (SELECT animal.VAT FROM animal);
+
+--6 (not working)
+SELECT avg(count(P.VAT_assistant)), avg(count(D.code)), avg(count(PR.name))
+FROM consult_diagnosis D, procedure PR, consult C, participation P
+WHERE YEAR(C.date_timestamp)=2017
+      AND D.date_timestamp=C.date_timestamp
+      AND P.date_timestamp = C.date_timestamp
+      AND PR.date_timestamp = C.date_timestamp;
