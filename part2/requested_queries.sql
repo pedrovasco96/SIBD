aa3e255e2c4599d5e02eeace7fea27287ec5a5db
Sara <scpvieira.96@gmail.com>
2018-11-08 22:21:28 +0000

    corrected query 9

--- part2/requested_queries.sql
+++ part2/requested_queries.sql
@@ -29,41 +29,44 @@ ORDER BY COUNT(DISTINCT medication.name) ASC;
 
 --6 (not working, possibly something with unions)
 SELECT count(P.VAT_assistant), count(distinct C.date_timestamp), count(D.code), count(PR.name), count(PC.date_timestamp)
-FROM consult_diagnosis D, proceeding PR, consult C, participation P, prescription PC
-WHERE YEAR(C.date_timestamp) = 2017
+FROM consult_diagnosis D,operation PR, consult C, participation P, prescription PC
+WHERE YEAR(C.date_timestamp) = 2017;
       AND YEAR(D.date_timestamp) = 2017
       AND YEAR(P.date_timestamp) = 2017
       AND YEAR(PC.date_timestamp) = 2017
       AND YEAR(PR.date_timestamp) = 2017;
 
-(SELECT count(distinct C.date_timestamp)
+(SELECT count(distinct C.date_timestamp) AS total_consults
 FROM consult C
 WHERE YEAR(C.date_timestamp)=2017)
 UNION
-(SELECT count(D.code)
+(SELECT count(*) AS total_diagnosis
 FROM consult_diagnosis D
-WHERE YEAR(D.date_timestamp)=2017)
+WHERE YEAR(D.date_timestamp)=2017);
 UNION
-(SELECT count(P.VAT_assistant)
+(SELECT count(*) AS assistants_participating
 FROM participation P
 WHERE YEAR(P.date_timestamp)=2017)
 UNION
-(SELECT count(PR.name)
-FROM proceeding PR
+(SELECT count(*) AS procedures_done
+FROM operation PR
 WHERE YEAR(PR.date_timestamp)=2017)
 UNION
-(SELECT count(PC.code)
+(SELECT count(*) AS prescriptions_given
 FROM prescription PC
 WHERE YEAR(PC.date_timestamp)=2017);
 
--- 7 (good for now, needs adjusting)
-SELECT S.name, C.name AS most_common
+
+-- 7 (good for now, needs adjusting to make sure it's not the same animal)
+SELECT S.name, C.name
 FROM generalization_species G, species S, animal A, consult_diagnosis D, diagnosis_code C
 WHERE G.name2 = 'Dog'
       AND G.name1 = S.name
       AND S.name = A.species_name
       AND A.name = D.name
-      AND D.code = C.code;
+      AND D.code = C.code
+GROUP BY S.name
+ORDER BY count(D.code) DESC;
 
 -- 8 (gives empty set, so it might work just fine :D)
 SELECT P.name
@@ -71,10 +74,11 @@ FROM person P, client C, veterinary V, assistant A, consult CO
 WHERE (P.VAT=C.VAT OR P.VAT=CO.VAT_client)
       AND (C.VAT=V.VAT OR C.VAT=A.VAT OR CO.VAT_client=V.VAT OR CO.VAT_client=A.VAT);
 
--- 9 (needs testing)
-SELECT P.name, P.address_street, P.address_city, P.address_zip
-FROM person P, client C, animal A, generalization_species G
+-- 9 (needs testing, needs iteration)
+SELECT P.name, P.address_street, P.address_zip, P.address_city
+FROM person P, client C, animal A, generalization_species G1, generalization_species G2
 WHERE P.VAT=C.VAT
-      AND C.VAT=A.VAT
-      AND A.species_name=G.name1
-      AND G.name2='Bird';
+      AND C.VAT=A.VAT_owner
+      AND A.species_name=G1.name1
+      AND G1.name2=G2.name1
+      AND G2.name2="Bird";
