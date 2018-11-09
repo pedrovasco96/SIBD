f1d669db80c88154bb7dc28b390c34275f9d4ff0
João Rodrigues <joaorodrigues481@gmail.com>
2018-11-09 22:46:09 +0000

    changed test_procedure to operation

--- part2/views/facts_consults.sql
+++ part2/views/facts_consults.sql
@@ -1,11 +1,11 @@
 -- VIEWS
 -- 3
 CREATE VIEW facts_consults AS
-(SELECT dim_animal.name, dim_animal.VAT_owner, dim_date.date_timestamp, count(distinct pro.num) as 'distinct procedures', count(distinct pre.med_name) 'distinct medications'
+(SELECT dim_animal.name, dim_animal.VAT_owner, dim_date.date_timestamp, count(distinct op.num) as 'distinct procedures', count(distinct pre.med_name) 'distinct medications'
 	FROM
 		dim_date, dim_animal, consult
-		left outer join test_procedure pro
-			on (pro.name, pro.VAT_owner, pro.date_timestamp) = (consult.name, consult.VAT_owner, consult.date_timestamp)
+		left outer join operation op
+			on (op.name, op.VAT_owner, op.date_timestamp) = (consult.name, consult.VAT_owner, consult.date_timestamp)
 		left outer join prescription pre
 			on (pre.name, pre.VAT_owner, pre.date_timestamp) = (consult.name, consult.VAT_owner, consult.date_timestamp)
 		where (consult.name, consult.VAT_owner) = (dim_animal.name, dim_animal.VAT_owner)
