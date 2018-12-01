4f88de09ebfc437122f284264b20078a4bc0b9e7
João Rodrigues <joaorodrigues481@gmail.com>
2018-12-01 12:35:34 +0000

    stored procedure working but probably not correctly implemented

--- part3/procedure.sql
+++ part3/procedure.sql
@@ -1,24 +1,38 @@
 delimiter //
 drop procedure if exists change_refs;
-drop table if exists prod;
+
 create procedure change_refs ()
   begin
 
-  declare @prod table(name varchar(30));
-  select P.indicator_name into @prod
-    from produced_indicator P, indicator I
-    where I.units='miligrams' and I.name=P.produced_indicator);
+  SET FOREIGN_KEY_CHECKS = 0;
+  update produced_indicator
+    set value=value/10
+    where produced_indicator.indicator_name IN (select I.name
+                                                from indicator I
+                                                where I.units='milligrams');
+  SET FOREIGN_KEY_CHECKS = 1;
+  update indicator
+    set units = 'centigrams', reference_value = reference_value/10
+    where indicator.units='milligrams';
+  end
+  //
+
+/*
+
+drop table if exists mod_indicators;
+create temporary table mod_indicators (name varchar(255));
+insert into mod_indicators select P.indicator_name
+  from produced_indicator P, indicator I
+  where I.units='milligrams' and I.name=P.indicator_name;
+
+  update indicator
+    set units = 'milligrams', reference_value = reference_value*10
+    where indicator.units='centigrams';
+  end
 
   update produced_indicator
     set value=value/10
-    where produced_indicator.indicator_name=(select *
-      from indicator, prod
-      where indicator.name=prod.name);
-  drop table if exists prod;
-
-  update indicator
-    set units = 'centigrams', reference_value = reference_value/10
-    where indicator.units='miligrams';
-
-  end
-//
+    where produced_indicator.indicator_name IN (select indicator.name
+      from indicator, mod_indicators
+      where indicator.name=mod_indicators.name);
+*/
