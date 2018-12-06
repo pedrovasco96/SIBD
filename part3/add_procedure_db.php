062fab3d36ba2d3eb865d66b31ba7864dd7d90cd
Sara <scpvieira.96@gmail.com>
2018-12-06 23:15:47 +0000

    corrections on add procedure

--- part3/add_procedure_db.php
+++ part3/add_procedure_db.php
@@ -38,78 +38,43 @@
         $VAT_owner=$row["VAT_owner"];
     }
 
-    $connection->beginTransaction();
+
 
     $sql = "insert into operation values (1,'$animal_name', '$VAT_owner', '$date_timestamp', 'Analysis');";
     $result = $connection->query($sql);
-    if(!$result)
-    {
-        $flag=1;
-        $connection->rollback();
-    }
 
     $sql = "insert into test_procedure values (1,'$animal_name', '$VAT_owner', '$date_timestamp', 'Blood');";
     $result = $connection->query($sql);
-    if(!$result)
-    {
-        $flag=1;
-        $connection->rollback();
-    }
 
     $sql = "insert into performed values (1,'$animal_name', '$VAT_owner', '$date_timestamp', '$VAT_assistant');";
     $result = $connection->query($sql);
-    if(!$result)
-    {
-        $flag=1;
-        $connection->rollback();
-    }
+
 
     $sql = "insert into produced_indicator values (1,'$animal_name', '$VAT_owner', '$date_timestamp', 'White Blood Cell Count', :white_cells);";
     $exec = $connection->prepare($sql);
     $exec->bindParam(':white_cells', $white_cells, PDO::PARAM_INT);
     $exec->execute();
-    if(!$exec)
-    {
-        $flag=1;
-        $connection->rollback();
-    }
+
 
     $sql = "insert into produced_indicator values (1,'$animal_name', '$VAT_owner', '$date_timestamp', 'Number of Lymphocytes', :lymphocytes);";
     $exec = $connection->prepare($sql);
     $exec->bindParam(':lymphocytes', $lymphocytes, PDO::PARAM_INT);
     $exec->execute();
-    if(!$exec)
-    {
-        $flag=1;
-        $connection->rollback();
-    }
+
 
     $sql = "insert into produced_indicator values (1,'$animal_name', '$VAT_owner', '$date_timestamp', 'Number of Neutrophils', :neutrophils);";
     $exec = $connection->prepare($sql);
     $exec->bindParam(':neutrophils', $neutrophils, PDO::PARAM_INT);
     $exec->execute();
-    if(!$exec)
-    {
-        $flag=1;
-        $connection->rollback();
-    }
+
 
     $sql = "insert into produced_indicator values (1,'$animal_name', '$VAT_owner', '$date_timestamp', 'Number of Monocytes', :monocytes);";
     $exec = $connection->prepare($sql);
     $exec->bindParam(':monocytes', $monocytes, PDO::PARAM_INT);
     $exec->execute();
-    if(!$exec)
-    {
-        $flag=1;
-        $connection->rollback();
-    }
 
-    if($flag==0)
-    {
-        $connection->commit();
-    }
 
-    echo("New Blood Test Inserted in Databse \n");
+    echo("New Blood Test Inserted in Database \n");
 
     echo("<button onclick=document.location.href=\"ini.html?flag=1\">Back to Initial Page</button>");
 
